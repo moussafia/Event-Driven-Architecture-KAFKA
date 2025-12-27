a9bac07d5013648d785062ac910499b911979fd6
moussafia <moussafia.mohammed1999@gmail.com>
2025-12-27 22:56:57 +0100

    Tester avec Kafka-console-producer et kafka-console-consumer

---
+++ src/main/java/ma/enset/kafkastreamspringcloud/events/PageEvent.java
@@ -0,0 +1,11 @@
+package ma.enset.kafkastreamspringcloud.events;
+
+import java.util.Date;
+
+public record PageEvent(
+        String name,
+        String user,
+        Date date,
+        long duration
+) {
+}
